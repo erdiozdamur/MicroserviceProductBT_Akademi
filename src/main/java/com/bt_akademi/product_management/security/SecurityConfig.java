2be32efc43214a9fa3200f74ef13c1d77be68bfa
erdiozdamur <erdiozdamur@gmail.com>
2022-11-27 10:46:51 +0300

    ver4

---
+++ src/main/java/com/bt_akademi/product_management/security/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.bt_akademi.product_management.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+    @Value("${service.security.secure-key-username}")
+    private String secureKeyUsername;
+
+    @Value("${service.security.secure-key-password}")
+    private String secureKeyPassword;
+
+}
